28d2fb0a068e1c7ff22b459917ec8d4428ba7f53
Shahar Sandhaus <shaharsandhaus@Shahars-MacBook-Pro.local>
2017-05-17 15:42:05 -0700

    Image load test

--- game.js
+++ game.js
@@ -1,6 +1,16 @@
 
 var model;
 var shaderProgram;
+var cubeTexture;
+
+function handleTextureLoaded(image, texture) {
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+	gl.generateMipmap(gl.TEXTURE_2D);
+	gl.bindTexture(gl.TEXTURE_2D, null);
+}
 
 function init() {
 	var verts = [
@@ -11,6 +21,11 @@ function init() {
 
 	model = createModel(verts, [2, 3]);
 	shaderProgram = createShader("shader-vs", "shader-fs", ["pos", "color"]);
+
+	cubeTexture = gl.createTexture();
+	var cubeImage = new Image();
+	cubeImage.onload = function() { handleTextureLoaded(cubeImage, cubeTexture); }
+	cubeImage.src = 'opengl_logo.png';
 }
 
 function draw() {
